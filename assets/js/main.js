90c79e2bed7052c7931ddb90a21658a0e4352ef3
Oliver Liongson <44100716+olsantiago@users.noreply.github.com>
2025-03-13 13:44:07 -0700

    add phone number track

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
-$(function () {
-    $("#icw").on("click", function () {
-        console.log("icw clicked");
-    });
+// only for AB Testing
+document.addEventListener("click", function (e) {
+  const intaker = e.target.closest("#icw");
+  const headerPhone = e.target.closest("#header-phone");
+
+  if (intaker) {
+    window.parent.postMessage("intaker_clicked", "*");
+  }
+  if (headerPhone) {
+    window.parent.postMessage("phone_number_clicked", "*");
+  }
 });
