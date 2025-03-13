2c8515a1ae6906cdeddb3264462ff18aac6e6f8f
Oliver Liongson <44100716+olsantiago@users.noreply.github.com>
2025-03-13 14:19:37 -0700

    clean up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,18 @@
-// only for AB Testing
-document.addEventListener("click", function (e) {
-  const intaker = e.target.closest("#icw");
-  const headerPhone = e.target.closest("#header-phone");
+$(function () {
+  window.addEventListener("hashchange", function () {
+    history.replaceState("", document.title, window.location.pathname + window.location.search);
+  });
 
-  if (intaker) {
-    window.parent.postMessage("intaker_clicked", "*");
-  }
-  if (headerPhone) {
-    window.parent.postMessage("phone_number_clicked", "*");
-  }
+  // only for AB Testing
+  document.addEventListener("click", function (e) {
+    const intaker = e.target.closest("#icw");
+    const headerPhone = e.target.closest("#header-phone");
+
+    if (intaker) {
+      window.parent.postMessage("intaker_clicked", "*");
+    }
+    if (headerPhone) {
+      window.parent.postMessage("phone_number_clicked", "*");
+    }
+  });
 });
